d5c47c7241110b0f149112780b229534cf40b791
Gaurav Ubnare <gauravubnare123@gmail.com>
2019-05-02 05:55:21 -0400

    Update php-error-detection.sh

--- mail-php-error-log-script/php-error-detection.sh
+++ mail-php-error-log-script/php-error-detection.sh
@@ -1,20 +1,15 @@
-echo "Please Enter a Log file Path"
-read log
-echo "Please Enter a Mail Addreess whom you want to send the error report"
-read mail
-## Script Setup for PHP Error Through Logs
-echo "error=$(tail -n 15 $log | grep -i GET)" > /root/error-detection.sh
-echo "$log" > /root/error-detection.sh
-#echo "echo $error >> /root/send-mail.txt" >> /root/error-detection.sh
-#echo "count=$(cat /root/send-mail.txt | wc -l)" >> /root/error-detection.sh
-echo 'if [ "$count" == "1" ]' >> /root/error-detection.sh
-echo 'then' >> /root/error-detection.sh
-echo '  mail -s "Error Detected" "$mail" < /root/send-mail.txt' >> /root/error-detection.sh
-echo 'else' >> /root/error-detection.sh
-echo '  echo "No Error"' >> /root/error-detection.sh
-echo 'fi' >> /root/error-detection.sh
 
-## Crontab Setup
+## Script Setup for PHP Error Through Logs
+error=$(tail -n 15 $logfilepath | grep -i GET)
+echo $error >> /root/send-mail.txt
+count=$(cat /root/send-mail.txt | wc -l)
+if [ "$count" == "1" ]
+then
+  mail -s "Error Detected"  < /root/send-mail.txt
+else 
+  echo "No Error"
+fi
+
 
 
 
