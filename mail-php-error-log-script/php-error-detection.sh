66e94c8a90a6c9d2f0699dfc765484222e4defeb
Gaurav Ubnare <gauravubnare123@gmail.com>
2019-05-03 09:59:38 -0400

    Update php-error-detection.sh

--- mail-php-error-log-script/php-error-detection.sh
+++ mail-php-error-log-script/php-error-detection.sh
@@ -1,17 +1,13 @@
-
-## Script Setup for PHP Error Through Logs
-error=$(tail -n 15 $logfilepath | grep -i GET)
-echo $error >> /root/send-mail.txt
+##Script to send error logs by mail
+error=$(tail -n 15 /var/log/httpderror_log | grep -i "Parse error")
+error2=$(tail -n 15 /var/log/httpderror_log | grep -i "fatal error")
+echo $error > /root/send-mail.txt
+echo " "  >> /root/send-mail.txt
+echo $error2 >> /root/send-mail.txt
 count=$(cat /root/send-mail.txt | wc -l)
 if [ "$count" == "1" ]
 then
-  mail -s "Error Detected"  $mail < /root/send-mail.txt
-else 
-  echo "No Error"
+ mail -s "Error Detected" abc@gmail.com < /root/send-mail.txt
+else
+ echo "No Error"
 fi
-
-
-
-
-
-
